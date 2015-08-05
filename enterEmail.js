c03892f8951631a59e8a727f872a75be34f11203
josephine-mei <josie.may.lo@gmail.com>
2015-08-05 10:53:28 -0400

    enterEmail
    Just created!

---
+++ enterEmail.js
@@ -0,0 +1,24 @@
+
+function CreateEmailLogin(input) {
+	for (var i = 0; i < input.length; i++) {
+		var emailDict = []; // create an empty array
+			if (!emailDict[input.key]) { //if email isn't in dict
+				e[email.key] = [];
+				e[email.key].push(email.val);
+					window.location = "success.html"; // Redirecting to other page.
+			else input == emailDict
+			print("You have already voted")
+		};
+	}
+}
+
+emailDict = {}
+CreateEmailLogin(emailDict)
+
+
+function CreateTextbox(){
+	
+	
+}
+
+
